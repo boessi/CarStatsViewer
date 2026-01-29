a7994c84c8a5b81fb76cdae5723d2f6a8adedeaf
boessi <dario.bosshard@flicks.ch>
2026-01-29 16:31:40 +0100

    adjust setting view

--- automotive/src/main/java/com/ixam97/carStatsViewer/utils/ChangeLogCreator.kt
+++ automotive/src/main/java/com/ixam97/carStatsViewer/utils/ChangeLogCreator.kt
@@ -1,6 +1,9 @@
 package com.ixam97.carStatsViewer.utils
 
+import android.app.ActivityManager
 import android.content.Context
+import android.os.Build
+import com.ixam97.carStatsViewer.CarStatsViewer
 import com.ixam97.carStatsViewer.R
 
 object ChangeLogCreator {
@@ -25,6 +28,8 @@ object ChangeLogCreator {
         var currentChanges = ""
 
         changesList.addAll(pendingChangesList)
+
+        versionsMap["System"] = getSystemInfo()
 
         changesList.reversed().forEachIndexed { index, change ->
             if (change.contains(versionRegex)) {
@@ -54,5 +59,56 @@ object ChangeLogCreator {
         var preReleaseVersion: String = ""
 
         return preReleaseVersion
+    }
+
+
+    private fun getSystemInfo(): String {
+        val sb = StringBuilder()
+
+        // Software
+        sb.append("--- SOFTWARE ---\n")
+        sb.append("Android Ver: ${Build.VERSION.RELEASE} (API ${Build.VERSION.SDK_INT})\n")
+        sb.append("Build ID: ${Build.ID}\n")
+        sb.append("Incremental: ${Build.VERSION.INCREMENTAL}\n")
+        sb.append("Codename: ${Build.VERSION.CODENAME}\n")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            sb.append("Sec. Patch: ${Build.VERSION.SECURITY_PATCH}\n")
+        }
+        sb.append("Tags: ${Build.TAGS}\n")
+        sb.append("Type: ${Build.TYPE}\n")
+        // Hardware
+        sb.append("\n--- HARDWARE ---\n")
+        sb.append("Manufacturer: ${Build.MANUFACTURER}\n")
+        sb.append("Brand: ${Build.BRAND}\n")
+        sb.append("Model: ${Build.MODEL}\n")
+        sb.append("Product: ${Build.PRODUCT}\n")
+        sb.append("Device: ${Build.DEVICE}\n")
+        sb.append("Board: ${Build.BOARD}\n")
+        sb.append("Hardware: ${Build.HARDWARE}\n")
+        sb.append("Architecture: ${System.getProperty("os.arch")}\n")
+
+        // Memory
+        sb.append("\n--- MEMORY ---\n")
+        val actManager = CarStatsViewer.appContext.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val memInfo = ActivityManager.MemoryInfo()
+        actManager.getMemoryInfo(memInfo)
+        val totalMem = memInfo.totalMem / (1024 * 1024)
+        val availMem = memInfo.availMem / (1024 * 1024)
+        sb.append("RAM: ${availMem}MB free / ${totalMem}MB total\n")
+
+        // Storage
+        val dataPath = android.os.Environment.getDataDirectory()
+        val stat = android.os.StatFs(dataPath.path)
+        val blockSize = stat.blockSizeLong
+        val totalStorage = (stat.blockCountLong * blockSize) / (1024 * 1024)
+        val freeStorage = (stat.availableBlocksLong * blockSize) / (1024 * 1024)
+        sb.append("Int. Storage: ${freeStorage}MB free / ${totalStorage}MB total\n")
+
+        // Graphics
+        val configInfo = actManager.deviceConfigurationInfo
+        sb.append("GLES Ver: ${configInfo.getGlEsVersion()}\n")
+
+
+        return sb.toString()
     }
 }
