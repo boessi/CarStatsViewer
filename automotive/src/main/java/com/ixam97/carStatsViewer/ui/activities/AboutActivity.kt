1a2f217e8dfd6f20cff12fdad4e521a6a7f5ab8e
boessi <dario.bosshard@flicks.ch>
2026-01-29 16:04:14 +0100

    Add Hardware View

--- automotive/src/main/java/com/ixam97/carStatsViewer/ui/activities/AboutActivity.kt
+++ automotive/src/main/java/com/ixam97/carStatsViewer/ui/activities/AboutActivity.kt
@@ -8,6 +8,9 @@ import androidx.fragment.app.FragmentActivity
 import com.ixam97.carStatsViewer.BuildConfig
 import com.ixam97.carStatsViewer.CarStatsViewer
 import com.ixam97.carStatsViewer.R
+import android.os.Build
+import android.content.Context
+import android.app.ActivityManager
 import com.ixam97.carStatsViewer.databinding.ActivityAboutBinding
 
 class AboutActivity : FragmentActivity() {
@@ -104,5 +107,84 @@ class AboutActivity : FragmentActivity() {
             }
             supportersDialog.show()
         }
+
+        binding.aboutSystemInfoWidget.setOnRowClickListener {
+            showSystemInfoDialog()
+        }
+    }
+
+    private fun showSystemInfoDialog() {
+        val systemInfo = getSystemInfo()
+        AlertDialog.Builder(this).apply {
+            setPositiveButton(getString(R.string.dialog_close)) { dialog, _ ->
+                dialog.cancel()
+            }
+            setTitle(getString(R.string.about_system_info_title))
+            setMessage(systemInfo)
+            setCancelable(true)
+            create()
+        }.show()
+    }
+
+    private fun getSystemInfo(): String {
+        val sb = StringBuilder()
+        
+        // Software
+        sb.append("--- SOFTWARE ---\n")
+        sb.append("Android Ver: ${Build.VERSION.RELEASE} (API ${Build.VERSION.SDK_INT})\n")
+        sb.append("Build ID: ${Build.ID}\n")
+        sb.append("Incremental: ${Build.VERSION.INCREMENTAL}\n")
+        sb.append("Codename: ${Build.VERSION.CODENAME}\n")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            sb.append("Sec. Patch: ${Build.VERSION.SECURITY_PATCH}\n")
+        }
+        sb.append("Tags: ${Build.TAGS}\n")
+        sb.append("Type: ${Build.TYPE}\n")
+        // Hardware
+        sb.append("\n--- HARDWARE ---\n")
+        sb.append("Manufacturer: ${Build.MANUFACTURER}\n")
+        sb.append("Brand: ${Build.BRAND}\n")
+        sb.append("Model: ${Build.MODEL}\n")
+        sb.append("Product: ${Build.PRODUCT}\n")
+        sb.append("Device: ${Build.DEVICE}\n")
+        sb.append("Board: ${Build.BOARD}\n")
+        sb.append("Hardware: ${Build.HARDWARE}\n")
+        sb.append("Architecture: ${System.getProperty("os.arch")}\n")
+        
+        // Memory
+        sb.append("\n--- MEMORY ---\n")
+        val actManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val memInfo = ActivityManager.MemoryInfo()
+        actManager.getMemoryInfo(memInfo)
+        val totalMem = memInfo.totalMem / (1024 * 1024)
+        val availMem = memInfo.availMem / (1024 * 1024)
+        sb.append("RAM: ${availMem}MB free / ${totalMem}MB total\n")
+
+        // Storage
+        val dataPath = android.os.Environment.getDataDirectory()
+        val stat = android.os.StatFs(dataPath.path)
+        val blockSize = stat.blockSizeLong
+        val totalStorage = (stat.blockCountLong * blockSize) / (1024 * 1024)
+        val freeStorage = (stat.availableBlocksLong * blockSize) / (1024 * 1024)
+        sb.append("Int. Storage: ${freeStorage}MB free / ${totalStorage}MB total\n")
+        
+        // Display
+        sb.append("\n--- DISPLAY ---\n")
+        val dm = resources.displayMetrics
+        val windowManager = getSystemService(Context.WINDOW_SERVICE) as android.view.WindowManager
+        val display = windowManager.defaultDisplay
+        sb.append("Resolution: ${dm.widthPixels}x${dm.heightPixels}\n")
+        sb.append("Density: ${dm.densityDpi} dpi\n")
+        sb.append("Refresh Rate: ${display.refreshRate} Hz\n")
+
+        // Graphics
+        val configInfo = actManager.deviceConfigurationInfo
+        sb.append("GLES Ver: ${configInfo.getGlEsVersion()}\n")
+
+        if (packageManager.hasSystemFeature(android.content.pm.PackageManager.FEATURE_AUTOMOTIVE)) {
+            sb.append("\nSystem Feature: Automotive\n")
+        }
+
+        return sb.toString()
     }
 }
